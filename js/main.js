bafbc426aedd9a5381260ca6a6726faded760905
M-12-10 <buttwahaj110@gmail.com>
2025-02-12 21:29:33 +0500

    as

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('For You Meri Jaan 💓').split('')
+    const titles = ('For You Meri Jaan 💌').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
